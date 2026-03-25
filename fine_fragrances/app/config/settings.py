7a485eeba511a0f115f0121061697e8fc65024c2
Rogaz111 <rayn@tad.co.za>
2026-03-25 22:20:35 +0200

    Initial Commit v2

--- fine_fragrances/app/config/settings.py
+++ fine_fragrances/app/config/settings.py
@@ -69,14 +69,11 @@ ASGI_APPLICATION = 'app.config.asgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fine_fragrance',
-        'USER': 'admin',
-        'PASSWORD': '0NwJbxVtbbzO4jH4CgP14GANemEj4xun',
-        'HOST': 'dpg-d718cr5m5p6s738gump0-a.oregon-postgres.render.com',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
