2377cc12c4302c7de50fdf4f30422a7a5e938ecb
Rogaz111 <rayn@tad.co.za>
2026-03-27 16:15:52 +0200

    added models and admin for all.

--- app/fragrance_api/admin.py
+++ app/fragrance_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from fragrance_api.models import Brand, ProductCategory, Product
+
 # Register your models here.
+admin.site.register(Brand)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(Brand)
--- app/fragrance_api/models.py
+++ app/fragrance_api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Timestamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class ProductCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(Timestamp):
+    id = models.AutoField(primary_key=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.brand.name} - {self.name}"
